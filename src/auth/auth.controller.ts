fc910b738d30b8a12deda21a7cda1d870df5681b
fahriz.dimasqy <fahriz.dimasqy@dwidasa.com>
2025-11-23 23:39:32 +0700

    schema owner can have multiple store and branches

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
 import { JwtAuthGuard } from 'src/utils/jwt-auth-guard.util';
 
+import { CREATED } from 'src/common/constant/operations.constant';
+import { CreateUserDto } from 'src/users/dto/user.dto';
+
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -11,6 +14,12 @@ export class AuthController {
   async login(@Body() dto: LoginDto) {
     return this.authService.login(dto.username, dto.password);
   }
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    await this.authService.register(dto);
+    return CREATED;
+  }
+
   @UseGuards(JwtAuthGuard)
   @Get('verify')
   verify() {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,21 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from 'src/users/dto/user.dto';
 
 @Injectable()
 export class AuthService {
+  private SALT_ROUNDS = 10;
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private prisma: PrismaService,
   ) {}
 
   async login(username: string, password: string) {
@@ -26,5 +34,18 @@ export class AuthService {
     return {
       access_token: token,
     };
+  }
+
+  async register(data: CreateUserDto) {
+    const exists = await this.prisma.user.findFirst({
+      where: { OR: [{ email: data.email }, { username: data.username }] },
+    });
+    if (exists)
+      throw new BadRequestException('Email atau username sudah dipakai');
+    const hashed = await bcrypt.hash(data.password, this.SALT_ROUNDS);
+
+    return this.prisma.user.create({
+      data: { ...data, password: hashed, role: 'OWNER' },
+    });
   }
 }
